bc5033265ed14cfe1ea9ce65de531c2c99e9c6dc
wei zheng <tobe.zheng.wei@gmail.com>
2018-10-28 11:59:10 +1100

    refactor the segmented control into a component according to the hosted platform, and also make the text input render according to platform.

---
+++ tabs/home/SegmentedControl.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { Platform } from 'react-native';
+import { SegmentedControlIOS } from 'react-native';
+import SegmentedControlTab from 'react-native-segmented-control-tab'
+
+
+export default class SegmentedControl extends Component {
+    render() {
+        if (Platform.OS == 'ios') {
+            return (
+                <SegmentedControlIOS
+                    style={{ marginBottom: 8 }}
+                    values={['Baby Formula', 'Supplements']}
+                    selectedIndex={this.props.productCatIndex}
+                    onChange={(event) => {
+                        this.props.callbackiOS(event);
+                    }}
+                />
+            );
+        } else if (Platform.OS == 'android') {
+            return (
+                <SegmentedControlTab
+                    tabStyle={{ marginBottom: 8 }}
+                    values={['Baby Formula', 'Supplements']}
+                    selectedIndex={this.props.productCatIndex}
+                    onTabPress={(index) => {
+                        this.props.callbackAndroid(index);
+                    }}
+                />
+            );
+        }
+    }
+}
